90318d000c3fd031155bd4eddd705e2de7373de6
Navee <your-email@example.com>
2026-02-02 22:53:35 +0530

    Fourth_change

--- src/constants/index.js
+++ src/constants/index.js
@@ -148,146 +148,146 @@ export const educationList = [
   },
 ];
 
-// Add your past achievements here for example - rankings in hackathons/events
-export const achievements = [
-  {
-    id: "a-1",
-    icon: ethindia,
-    event: "ETHIndia'24 | India's Largest Ethereum Hackathon",
-    position: "Winner",
-    content1: "Top 10 Overall Finalists/Winners, True Network's Winner",
-    content2: "Polkadot's 1st Runner Up, Lit Protocol's 2nd Runner Up",
-    content3: "Coinbase Pool Prize, Base Top 10",
-    article:
-      "https://www.linkedin.com/posts/mittal-parth_super-stoked-to-announce-that-our-team-emerged-activity-7274735259621961729-tkq4?utm_source=share&utm_medium=member_desktop",
-    project: "https://devfolio.co/projects/khoj-3336",
-    youtube:
-      "https://www.youtube.com/live/qJ4OCtnvjUY?si=VkcnHEdwJTEEDlMg&t=4718",
-  },
-  {
-    id: "a-2",
-    icon: ethglobal,
-    event: "ETHIndia'22 | World's Largest Ethereum Hackathon",
-    position: "Winner",
-    content1: "Top 12 Winners among 20k+ registrations",
-    content2: "One of Polygon's Best Public Goods",
-    content3: "Best Module on Biconomy SDK",
-    article:
-      "https://www.thehindu.com/news/cities/Mangalore/nitk-iiit-delhi-team-makes-it-to-top-12-winners-in-ethindia-22/article66238923.ece",
-    project: "https://devfolio.co/projects/chargeswap-3527",
-    youtube: "https://youtu.be/9rieTya8Yds?t=3908",
-  },
-  {
-    id: "a-3",
-    icon: polkadot,
-    event: "Polkadot Hackathon: Europe Edition",
-    position: "2nd Runner Up in the ink! Smart Contract Category",
-    content1:
-      "Built GreenTrust offering a novel solution for obtaining certification in organic farming.",
-    content2: "",
-    content3: "",
-    article:
-      "https://www.linkedin.com/posts/mittal-parth_hackathon-winners-web3-activity-7048340759116214272-eJvo?utm_source=share&utm_medium=member_desktop&rcm=ACoAADOa76QB7zljgX8NK4xVSZXi0Z49ETMeC-g",
-    github: "https://github.com/pranav2305/GreenTrust",
-  },
-  {
-    id: "a-4",
-    icon: lightspeed,
-    event: "Warpspeed by Lightspeed 2023",
-    position: "1st Runner Up",
-    content1: "1st Runner Up Overall by Lightspeed among 107 hackers",
-    content2: "Top 3 projects using Replit",
-    content3: "1st Runner Up by Amazon Web Services (AWS)",
-    article: "https://shorturl.at/fhjsT",
-  },
-  {
-    id: "a-5",
-    icon: lightspeed,
-    event: "Warpspeed: Agentic AI Hackathon | Lightspeed India",
-    position: "Runners Up in the Base Track",
-    content1: "Built an ambient virtual assistant before ChatGPT Pulse",
-    content2: "Runners up in the Base Track with the Agentic AI theme for 2025",
-    project: "https://devfolio.co/projects/aeva-58d2",
-  },
-  {
-    id: "a-6",
-    icon: kudos,
-    event: "Kudos Carnival | Polkadot Blockchain Academy",
-    position: "Runner Up",
-    content1: "Finished 2nd globally among PBA Alumni in a 6-week event",
-    content2: "Contributed to several repositories in the Polkadot ecosystem in a Hacktoberfest-style event",
-    article: "https://www.morekudos.com/carnival",
-  },
-  {
-    id: "a-7",
-    icon: web3_marketing_hackathon,
-    event: "Web3 Marketing Hackathon",
-    position: "Runner Up",
-    content1:
-      "2nd position in the Polkadot Challenge I - Creative Marketing Strategies to Boost Polkadot Awareness & Adoption",
-    content2: "",
-    content3: "",
-    article: "https://x.com/polkadotsub0/status/1998425721916551355?s=46",
-    project: "https://taikai.network/OutofOrdinary/hackathons/web3mkthack/projects/cmi0skdbk0257vu09q3n8m44u/idea",
-  },
-  {
-    id: "a-8",
-    icon: globalAi,
-    event: "Global AI HackFest 2023",
-    position: "Winner",
-    content1:
-      "1st Position in the Education, Finance & Tech track by AI Planet",
-    content2: "",
-    content3: "",
-    article:
-      "https://www.linkedin.com/posts/mittal-parth_happy-to-share-that-comicifyai-emerged-as-activity-7078790186435833856-88fh",
-  },
-  {
-    id: "a-9",
-    icon: dennisivy,
-    event: "September Hackathon by Dennis Ivy",
-    position: "Winner",
-    content1:
-      "Rated the best portfolio website among 450+ participants across the globe.",
-    content2: "",
-    content3: "",
-    youtube: "https://www.youtube.com/watch?v=X2473En3h_o&t=5278s",
-    project: "https://parthmittal.netlify.app/",
-  },
-  {
-    id: "a-10",
-    icon: manipal,
-    event: "Manipal Hackathon'22",
-    position: "Consolation Prize",
-    content1: "Top 10 among 500+ teams across India",
-    content2:
-      "Developed a cross-platform mobile application to address the problem of social cohesion.",
-    content3: "",
-    article: "https://shorturl.at/exEIQ",
-  },
-  {
-    id: "a-11",
-    icon: icon,
-    event: "ICON Hyperbuild Hackathon",
-    position: "Honorable Mention",
-    content1:
-      "Honorable Mention among 655 participants across the globe in a 3 month-long online hackathon.",
-    content2: "",
-    content3: "",
-    project: "https://devpost.com/software/green-trust-xj2w6g",
-  },
-  {
-    id: "a-12",
-    icon: ethforall,
-    event: "ETHForAll 2023",
-    position: "Top 3 Superfluid Projects",
-    content1:
-      "Bounty winners among 430 projects in ETHGlobal's largest online hackathon.",
-    content2: "",
-    content3: "",
-    project: "https://devfolio.co/projects/green-trust-ed14",
-  },
-];
+// // Add your past achievements here for example - rankings in hackathons/events
+// export const achievements = [
+//   {
+//     id: "a-1",
+//     icon: ethindia,
+//     event: "ETHIndia'24 | India's Largest Ethereum Hackathon",
+//     position: "Winner",
+//     content1: "Top 10 Overall Finalists/Winners, True Network's Winner",
+//     content2: "Polkadot's 1st Runner Up, Lit Protocol's 2nd Runner Up",
+//     content3: "Coinbase Pool Prize, Base Top 10",
+//     article:
+//       "https://www.linkedin.com/posts/mittal-parth_super-stoked-to-announce-that-our-team-emerged-activity-7274735259621961729-tkq4?utm_source=share&utm_medium=member_desktop",
+//     project: "https://devfolio.co/projects/khoj-3336",
+//     youtube:
+//       "https://www.youtube.com/live/qJ4OCtnvjUY?si=VkcnHEdwJTEEDlMg&t=4718",
+//   },
+//   {
+//     id: "a-2",
+//     icon: ethglobal,
+//     event: "ETHIndia'22 | World's Largest Ethereum Hackathon",
+//     position: "Winner",
+//     content1: "Top 12 Winners among 20k+ registrations",
+//     content2: "One of Polygon's Best Public Goods",
+//     content3: "Best Module on Biconomy SDK",
+//     article:
+//       "https://www.thehindu.com/news/cities/Mangalore/nitk-iiit-delhi-team-makes-it-to-top-12-winners-in-ethindia-22/article66238923.ece",
+//     project: "https://devfolio.co/projects/chargeswap-3527",
+//     youtube: "https://youtu.be/9rieTya8Yds?t=3908",
+//   },
+//   {
+//     id: "a-3",
+//     icon: polkadot,
+//     event: "Polkadot Hackathon: Europe Edition",
+//     position: "2nd Runner Up in the ink! Smart Contract Category",
+//     content1:
+//       "Built GreenTrust offering a novel solution for obtaining certification in organic farming.",
+//     content2: "",
+//     content3: "",
+//     article:
+//       "https://www.linkedin.com/posts/mittal-parth_hackathon-winners-web3-activity-7048340759116214272-eJvo?utm_source=share&utm_medium=member_desktop&rcm=ACoAADOa76QB7zljgX8NK4xVSZXi0Z49ETMeC-g",
+//     github: "https://github.com/pranav2305/GreenTrust",
+//   },
+//   {
+//     id: "a-4",
+//     icon: lightspeed,
+//     event: "Warpspeed by Lightspeed 2023",
+//     position: "1st Runner Up",
+//     content1: "1st Runner Up Overall by Lightspeed among 107 hackers",
+//     content2: "Top 3 projects using Replit",
+//     content3: "1st Runner Up by Amazon Web Services (AWS)",
+//     article: "https://shorturl.at/fhjsT",
+//   },
+//   {
+//     id: "a-5",
+//     icon: lightspeed,
+//     event: "Warpspeed: Agentic AI Hackathon | Lightspeed India",
+//     position: "Runners Up in the Base Track",
+//     content1: "Built an ambient virtual assistant before ChatGPT Pulse",
+//     content2: "Runners up in the Base Track with the Agentic AI theme for 2025",
+//     project: "https://devfolio.co/projects/aeva-58d2",
+//   },
+//   {
+//     id: "a-6",
+//     icon: kudos,
+//     event: "Kudos Carnival | Polkadot Blockchain Academy",
+//     position: "Runner Up",
+//     content1: "Finished 2nd globally among PBA Alumni in a 6-week event",
+//     content2: "Contributed to several repositories in the Polkadot ecosystem in a Hacktoberfest-style event",
+//     article: "https://www.morekudos.com/carnival",
+//   },
+//   {
+//     id: "a-7",
+//     icon: web3_marketing_hackathon,
+//     event: "Web3 Marketing Hackathon",
+//     position: "Runner Up",
+//     content1:
+//       "2nd position in the Polkadot Challenge I - Creative Marketing Strategies to Boost Polkadot Awareness & Adoption",
+//     content2: "",
+//     content3: "",
+//     article: "https://x.com/polkadotsub0/status/1998425721916551355?s=46",
+//     project: "https://taikai.network/OutofOrdinary/hackathons/web3mkthack/projects/cmi0skdbk0257vu09q3n8m44u/idea",
+//   },
+//   {
+//     id: "a-8",
+//     icon: globalAi,
+//     event: "Global AI HackFest 2023",
+//     position: "Winner",
+//     content1:
+//       "1st Position in the Education, Finance & Tech track by AI Planet",
+//     content2: "",
+//     content3: "",
+//     article:
+//       "https://www.linkedin.com/posts/mittal-parth_happy-to-share-that-comicifyai-emerged-as-activity-7078790186435833856-88fh",
+//   },
+//   {
+//     id: "a-9",
+//     icon: dennisivy,
+//     event: "September Hackathon by Dennis Ivy",
+//     position: "Winner",
+//     content1:
+//       "Rated the best portfolio website among 450+ participants across the globe.",
+//     content2: "",
+//     content3: "",
+//     youtube: "https://www.youtube.com/watch?v=X2473En3h_o&t=5278s",
+//     project: "https://parthmittal.netlify.app/",
+//   },
+//   {
+//     id: "a-10",
+//     icon: manipal,
+//     event: "Manipal Hackathon'22",
+//     position: "Consolation Prize",
+//     content1: "Top 10 among 500+ teams across India",
+//     content2:
+//       "Developed a cross-platform mobile application to address the problem of social cohesion.",
+//     content3: "",
+//     article: "https://shorturl.at/exEIQ",
+//   },
+//   {
+//     id: "a-11",
+//     icon: icon,
+//     event: "ICON Hyperbuild Hackathon",
+//     position: "Honorable Mention",
+//     content1:
+//       "Honorable Mention among 655 participants across the globe in a 3 month-long online hackathon.",
+//     content2: "",
+//     content3: "",
+//     project: "https://devpost.com/software/green-trust-xj2w6g",
+//   },
+//   {
+//     id: "a-12",
+//     icon: ethforall,
+//     event: "ETHForAll 2023",
+//     position: "Top 3 Superfluid Projects",
+//     content1:
+//       "Bounty winners among 430 projects in ETHGlobal's largest online hackathon.",
+//     content2: "",
+//     content3: "",
+//     project: "https://devfolio.co/projects/green-trust-ed14",
+//   },
+// ];
 
 // Add your software developments skills here for example - programming languages, frameworks etc.
 export const skills = [
